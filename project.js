ee3b93f4ce0203984a447d471094117bc9a10b0d
Bryce Rubinson <brycerubinson@gmail.com>
2019-08-27 10:11:18 -0400

    button variant color

--- project.js
+++ project.js
@@ -1,3 +1,33 @@
+path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
 module.exports = require("marko-starter").projectConfig({
-  name: "marko-starter-demo" // Optional, but added here for demo purposes
+  name: "Lemon Marko Demo", // Optional, but added here for demo purposes
+  lassoConfig: {
+    plugins: [
+      'lasso-marko',
+      {
+        plugin: 'lasso-stylus',
+        config: {
+            includes: [path.join(__dirname, 'src/stylus/mixins/'),path.join(__dirname,'src/components/**/*')],
+            use: function(stylus) {
+                stylus.define('add', function(a, b) {
+                    a = parseFloat(a);
+                    b = parseFloat(b);
+                    return a+b;
+                });
+            },
+            define: {
+                sub: function(a, b) {
+                    a = parseFloat(a);
+                    b = parseFloat(b);
+                    return a-b;
+                }
+            },
+            imports: [
+                path.join(__dirname, 'src/stylus/variables.styl')
+            ]
+        }
+    }
+    ]
+}
 });
