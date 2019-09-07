74c20108521b4a9f4ba4af3ecd12d0359b8d2a14
Bryce Rubinson <brycerubinson@gmail.com>
2019-09-07 16:10:29 -0400

    pre marko deleteions

--- src/components/Dropdown/Dropdown.component.js
+++ src/components/Dropdown/Dropdown.component.js
@@ -1,5 +1,18 @@
 module.exports = class {
+    onCreate() {
+        this.state = {
+            blurTimeout: null
+        }
+    }
     handleClick() {
         this.emit('click')
+    }
+    handleMenuItemClick(cb) {
+        this.emit('item-click',{
+            cb:cb
+        })
+    }
+    handleBlur() {
+        console.log('blurbed')
     }
 }
--- src/components/DropdownButton/DropdownButton.component.js
+++ src/components/DropdownButton/DropdownButton.component.js
@@ -7,4 +7,11 @@ module.exports = class {
     handleClick() {
         this.state.show = !this.state.show
     }
+    handleItemClick(data) {
+        this.emit('item-click',data)
+        this.state.show = false
+    }
+    handleBlur() {
+        console.log('blurbed')
+    }
 }
