ceb3f9c995956595ae67826ff07ed681586b236d
Bryce Rubinson <brycerubinson@gmail.com>
2019-09-09 12:36:14 -0400

    gh page build script

---
+++ buildGHdemo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo "Startinging build for GH pages"
+echo "Clearing dist and cache and old docs"
+rm -rf dist .cache docs
+
+echo "Building dist"
+yarn build
+
+echo "Renaming to Docs"
+mv dist docs
+
+echo "Moving demo route css"
+rm -rf ./docs/index.html && mv ./docs/demo/index.html ./docs/index.html
+echo "fixing paths"
+sed -i '' 's/src=\"\/demo/src=\".\/demo/g' ./docs/index.html
+sed -i '' 's/href=\"\/demo/href=\".\/demo/g' ./docs/index.html
+# mv ./docs/*demo* ./docs/demo/
+echo "Done!"
